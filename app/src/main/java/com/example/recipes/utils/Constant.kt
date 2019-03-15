5e5c1432b9ddcef065823529945966edc7b7faf0
HelAsia <asiaheluszka1993@gmail.com>
2019-03-15 16:26:27 +0100

    Connection with remote server

--- app/src/main/java/com/example/recipes/utils/Constant.kt
+++ app/src/main/java/com/example/recipes/utils/Constant.kt
@@ -2,7 +2,7 @@ package com.example.recipes.utils
 
 class Constant {
     companion object {
-        const val BASE_URL: String = "http://10.0.2.2:5000/ "
+        const val BASE_URL: String = "http://helasia.com:5000/"
         const val GLOBAL_TIMEOUT_PARAMETER: Long = 360
         const val USER_TYPE: String = "USER_TYPE"
     }
